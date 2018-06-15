dc9468187509a198097ddbcb1957df64f70fa736
Arctric1 <40288622+Arctric1@users.noreply.github.com>
2018-06-15 00:23:57 -0600

    Add files via upload
    wafas

---
+++ config/config.php
@@ -0,0 +1,16 @@
+<?php
+    
+    # Your MySQL details of the database that is connected to your server running LeaderHeads.
+    define('database_host', 'host');
+    define('database_port', '3306');
+    define('database_username', 'username');
+    define('database_password', 'password');
+    define('database_database', 'database');
+    
+    # Your server timezone.
+    # Check here for a full list of timezones available: http://php.net/manual/en/timezones.php
+    define('timezone', 'UTC');
+    
+    # Whether debug should be enabled or not.
+    define('debug', true);
+?>
